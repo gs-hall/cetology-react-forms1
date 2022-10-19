0563acabe34836cc1a29699a465cc05ced630275
Egor Ivantsov <E.Ivantsov@mobilepark.ru>
2022-10-19 23:02:46 +0300

    Fixes

--- src/ColorForm.js
+++ src/ColorForm.js
@@ -3,7 +3,7 @@ import "./main.css";
 
 export default function ColorForm() {
   const [form, setForm] = useState({
-    color: '#ffffff',
+    color: '#123456',
   });
 
   const handleFormChange = (e) => {
@@ -13,18 +13,27 @@ export default function ColorForm() {
     }));
   };
 
-  const changeBackgroundColor = (hex) => {
+  const setBackgroundColor = (hex) => {
+    if (hex == null) {return};
     document.documentElement.style.background = '#'+hex;
   }
 
-  const validHexColor = (hex) => {
+  /*
+  Returns:
+    - true if the color is ok,
+    - false if the color has 7 characters, but not ok
+    - null if the color cannot be validated (it is null or empty or its length is not 7)
+  */
+  const validHexColor = (color) => {
+    if (color == null || typeof color !== 'string' || color.length !== 7) {return null};
     const validHex = new RegExp('^#([0-9a-f]{6})$','i');
-    var match = validHex.exec(hex);
-    return match ? match[1] : null;
+    var match = validHex.exec(color);
+    return match ? match[1] : false;
   }
 
   const hex2rgb = (hex) => {
-    if (!hex) {return 'Ошибка'};
+    if (hex == null) {return 'Введите код (например: #123456)'};
+    if (!hex) {return 'Ошибка: неверный код'};
     hex = '0x' + hex;
     let r = (hex >> 16) & 0xFF;
     let g = (hex >> 8) & 0xFF;
@@ -34,16 +43,13 @@ export default function ColorForm() {
 
   // Render
   const hex = validHexColor(form.color);
-  if (hex) {
-    changeBackgroundColor(hex);
-  };
+  setBackgroundColor(hex);
 
   return (
     <div className="color-form">
-      <form className="color-form" onSubmit={(e) => {e.preventDefault()}}>
+      <form onSubmit={(e) => {e.preventDefault()}}>
         <div>
           <input
-            className="color-form"
             name="color"
             value={form.color}
             placeholder="#123456"
@@ -51,9 +57,8 @@ export default function ColorForm() {
             autoFocus
           />
         </div>
-        <p className={"color-form"+(hex ? '':' error')}>{hex2rgb(hex)}</p>
+        <p className={(hex === false ? ' error':null)}>{hex2rgb(hex)}</p>
       </form>
     </div>
   );
- //        <input className="color-form" name="rgb" value={rgb} readOnly />
 }
